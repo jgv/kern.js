558e1dafc6a05fc6cc9399270c8f1b283a51c714
Brendan Stromberger <brendanstromberger@gmail.com>
2011-02-21 01:14:02 -0800

    Need to figure out how to keep a particular letter engaged so one can perform the letter adjustment keyboard operations

--- kerning.js
+++ kerning.js
@@ -7,10 +7,23 @@ $(function() {
 			$(this).css('opacity', '1');
 			console.log(this);
 			$(this).click(function(event) {
-				
+				var engaged = true;
+				$(document.body).keydown(function(event) {
+					if(event.which == 37)
+					{
+						console.log(event.which);
+					}
+					if(event.which == 39)
+					{
+						console.log(event.which);
+					}
+				});
 			});
 		}).mouseleave(function() {
 			$(this).css('opacity', '.5');
 		});
 	});
 });
+
+// 37 is left
+// 39 is right
