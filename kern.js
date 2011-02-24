491abdbf766d3760573c740454cca13e44f8263a
Brendan Stromberger <brendanstromberger@gmail.com>
2011-02-24 14:34:44 -0800

    Confused. My script is trying to load before jQuery is fully loaded.

--- kern.js
+++ kern.js
@@ -1,11 +1,18 @@
-$(function() {
+if (typeof jQuery == 'undefined') {
+	var includejquery = document.createElement("script");
+	includejquery.src = "http://ajax.googleapis.com/ajax/libs/jquery/1.5/jquery.min.js";
+	// var html_doc = document.getElementsByTagName('head').item(0);
+	document.body.appendChild(includejquery);
+}
+
+jQuery.noConflict()(function() {
 	var activeEl, unit, increment, kerning, adjustments, thePanel;
 	kerning = 0;
 	adjustments = {};
 	thePanel =
 		['<style>',
-			'#kernjs_panel { height: auto; width: 290px; padding: 30px; -moz-border-radius: 0 0 0 10px; -webkit-border-radius: 0 0 0 10px; border-radius: 0 0 0 10px; position: absolute; right: 0; top: 0; background: black }',
-			'#kernjs_panel textarea { -moz-border-radius: 10px; -webkit-border-radius: 10px; border-radius: 10px; width: 280px; height: 300px; padding: 10px;',
+			'#kernjs_panel { height: 60px; width: 100%; margin: 0 auto; background: black }',
+			'#kernjs_panel textarea { -moz-border-radius: 10px; -webkit-border-radius: 10px; border-radius: 10px; width: 280px; height: 30px; ',
 		'</style>',
 
 		'<div id="kernjs_panel">',
@@ -15,39 +22,39 @@ $(function() {
 			'</form>',
 		'</div>'
 	].join('\n');
-	
-	$(document.body).append(thePanel);
-	
-	$("h1, h2, h3, h4, h5, h6").click(function(event) { // Activate a word
+
+	jQuery(document.body).prepend(thePanel);
+
+	jQuery("h1, h2, h3, h4, h5, h6").click(function(event) { // Activate a word
 		var el = event.target;
-		$(el).lettering(); // Call method from Lettering.js. This method splits up the clicked body of text into <span> elements containing single letters.
-		$(el).children().css('opacity', '.5');
-		$(el).children().mouseover(function() {
-			$(this).css('opacity', '1');
-			$(this).click(function(event) { // Listens for clicks on the newly created span objects.
+		jQuery(el).lettering(); // Call method from Lettering.js. This method splits up the clicked body of text into <span> elements containing single letters.
+		jQuery(el).children().css('opacity', '.5');
+		jQuery(el).children().mouseover(function() {
+			jQuery(this).css('opacity', '1');
+			jQuery(this).click(function(event) { // Listens for clicks on the newly created span objects.
 				activeEl = event.target; // Set activeEl to represent the clicked letter.
-				$(activeEl).css('color', 'red');
+				jQuery(activeEl).css('color', 'red');
 			}); // end el click		
 		});
 		kerning = 0;
 	});
-	
-	$(document).keydown(function(event) {
-		if(adjustments[$(activeEl).attr("id")]) { // If there are current adjustments already made for this letter
-			kerning = adjustments[$(activeEl).attr("id")]; // Set the kerning variable to the previously made adjustments for this letter (stored inside the adjustments dictionary object)
+
+	jQuery(document).keydown(function(event) {
+		if(adjustments[jQuery(activeEl).attr("id")]) { // If there are current adjustments already made for this letter
+			kerning = adjustments[jQuery(activeEl).attr("id")]; // Set the kerning variable to the previously made adjustments for this letter (stored inside the adjustments dictionary object)
 		}
 		if(event.which === 37) { // If left arrow key
 			kerning--;
-			$(activeEl).css('margin-left', kerning);
-			adjustments[$(activeEl).attr("id")] = kerning; // add/modify the current letter's kerning information to the "adjustments" object.
-			$("#kernjs_panel textarea").val(generateCSS(adjustments, unit, increment));
+			jQuery(activeEl).css('margin-left', kerning);
+			adjustments[jQuery(activeEl).attr("id")] = kerning; // add/modify the current letter's kerning information to the "adjustments" object.
+//			jQuery("#kernjs_panel textarea").val(generateCSS(adjustments, unit, increment));
 		}
 		if(event.which === 39) { // If right arrow key
 			kerning++;
-			$(activeEl).css('margin-left', kerning);
-			adjustments[$(activeEl).attr("id")] = kerning; // add/modify the current letter's kerning information to the "adjustments" object.
+			jQuery(activeEl).css('margin-left', kerning);
+			adjustments[jQuery(activeEl).attr("id")] = kerning; // add/modify the current letter's kerning information to the "adjustments" object.
 			generateCSS(adjustments, unit, increment);
-			$("#kernjs_panel textarea").val(generateCSS(adjustments, unit, increment));
+//			jQuery("#kernjs_panel textarea").val(generateCSS(adjustments, unit, increment));
 		}
 	});
 });
