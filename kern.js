e4ced80247483ce2927312452f27ceaa311cd34a
Brendan Stromberger <brendanstromberger@gmail.com>
2011-02-24 17:06:20 -0800

    UI work

--- kern.js
+++ kern.js
@@ -2,7 +2,7 @@ if (typeof jQuery == 'undefined') {
 	var includejquery = document.createElement("script");
 	includejquery.src = "http://ajax.googleapis.com/ajax/libs/jquery/1.5/jquery.min.js";
 	// var html_doc = document.getElementsByTagName('head').item(0);
-	document.body.appendChild(includejquery);
+	document.body.appendChild(includejquery);;
 	includejquery.onload = kern;
 }
 
@@ -12,15 +12,18 @@ function kern() {
 	adjustments = {};
 	thePanel =
 		['<style>',
-			'#kernjs_panel { height: 60px; width: 100%; margin: 0 auto; background: black }',
-			'#kernjs_panel textarea { -moz-border-radius: 10px; -webkit-border-radius: 10px; border-radius: 10px; width: 280px; height: 30px; ',
+			'#kernjs_panel { text-align: center; height: 60px; width: 100%; margin: 0 auto; background: black; font-family: \'Lucida Grande\', \'Helvetica Neue\', Helvetica, arial; }',
+			'#kernjs_panel textarea { -moz-border-radius: 10px; -webkit-border-radius: 10px; border-radius: 10px; width: 280px; height: 30px; }',
+			'#kernjs_panel ul { list-style-type: none; display: inline-block; }',
+			'#kernjs_panel li { display: inline-block; float: left; padding-right: 2em; }',
+			'#kernjs_panel a { color: white; }',
 		'</style>',
 
 		'<div id="kernjs_panel">',
-			'<form>',
-				'<textarea>',
-				'</textarea>',
-			'</form>',
+		'<ul>',
+			'<li><a href="#">Copy to Clipboard</a></li>',
+			'<li><a href="#">Save to File</a></li>',
+			'<li><a href="#">Return to editing</a></li>',
 		'</div>'
 	].join('\n');
 
@@ -28,14 +31,16 @@ function kern() {
 
 	jQuery("h1, h2, h3, h4, h5, h6").click(function(event) { // Activate a word
 		var el = event.target;
+		var previousColor = 0;
 		splitter(jQuery(el)); // Call method from Lettering.js. This method splits up the clicked body of text into <span> elements containing single letters.
 		jQuery(el).children().css('opacity', '.5');
 		jQuery(el).children().mouseover(function() {
-			jQuery(this).css('opacity', '1');
 			jQuery(this).click(function(event) { // Listens for clicks on the newly created span objects.
+				if(previousColor>0) jQuery(activeEl).css('color', previousColor);
 				activeEl = event.target; // Set activeEl to represent the clicked letter.
+				previousColor = jQuery(activeEl).css('color');
 				jQuery(activeEl).css('color', 'red');
-			}); // end el click		
+			}); // end el click	
 		});
 		kerning = 0;
 	});
@@ -71,14 +76,13 @@ function generateCSS(adjustments, unit, increment) {
 				'}'
 				].join('\n');
 				theCSS = theCSS + '\n' + concatCSS;
-				console.log(theCSS);
+//				console.log(theCSS);
 		}
 	}
 	return theCSS;
 }
 
 function splitter(el) {
-	console.log(el);
 	return el.each(function() {
 		injector(jQuery(el), '', 'char', '');
 	});
