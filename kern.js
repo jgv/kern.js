3d0fdc505489b8d331390611953c704d3d7d1287
Brendan Stromberger <brendanstromberger@gmail.com>
2011-02-24 20:40:48 -0800

    Need to add adjustment via mouse now

--- kern.js
+++ kern.js
@@ -1,9 +1,12 @@
-if (typeof jQuery == 'undefined') {
+if (typeof jQuery === 'undefined') {
 	var includejquery = document.createElement("script");
 	includejquery.src = "http://ajax.googleapis.com/ajax/libs/jquery/1.5/jquery.min.js";
 	// var html_doc = document.getElementsByTagName('head').item(0);
-	document.body.appendChild(includejquery);;
+	document.body.appendChild(includejquery);
 	includejquery.onload = kern;
+}
+else {
+	kern();
 }
 
 function kern() {
@@ -12,13 +15,12 @@ function kern() {
 	adjustments = {};
 	thePanel =
 		['<style>',
-			'#kernjs_panel { text-align: center; height: 60px; width: 100%; margin: 0 auto; background: black; font-family: \'Lucida Grande\', \'Helvetica Neue\', Helvetica, arial; }',
+			'#kernjs_panel { text-align: center; height: 55px; width: 100%; margin: 0 auto; background: black; font-family: \'Lucida Grande\', \'Helvetica Neue\', Helvetica, arial; }',
 			'#kernjs_panel textarea { -moz-border-radius: 10px; -webkit-border-radius: 10px; border-radius: 10px; width: 280px; height: 30px; }',
 			'#kernjs_panel ul { list-style-type: none; display: inline-block; }',
 			'#kernjs_panel li { display: inline-block; float: left; padding-right: 2em; }',
 			'#kernjs_panel a { color: white; }',
 		'</style>',
-
 		'<div id="kernjs_panel">',
 		'<ul>',
 			'<li><a href="#">Copy to Clipboard</a></li>',
@@ -28,19 +30,20 @@ function kern() {
 	].join('\n');
 
 	jQuery(document.body).prepend(thePanel);
-
+	
 	jQuery("h1, h2, h3, h4, h5, h6").click(function(event) { // Activate a word
 		var el = event.target;
 		var previousColor = 0;
+		jQuery(this).attr('unselectable', 'on').css('-moz-user-select', 'none').each(function() { this.onselectstart = function() { return false; }; } );
 		splitter(jQuery(el)); // Call method from Lettering.js. This method splits up the clicked body of text into <span> elements containing single letters.
 		jQuery(el).children().css('opacity', '.5');
 		jQuery(el).children().mouseover(function() {
 			jQuery(this).click(function(event) { // Listens for clicks on the newly created span objects.
-				if(previousColor>0) jQuery(activeEl).css('color', previousColor);
+				if(previousColor!==0) { jQuery(activeEl).css('color', previousColor); }
 				activeEl = event.target; // Set activeEl to represent the clicked letter.
 				previousColor = jQuery(activeEl).css('color');
 				jQuery(activeEl).css('color', 'red');
-			}); // end el click	
+			}); // end el click
 		});
 		kerning = 0;
 	});
@@ -53,14 +56,13 @@ function kern() {
 			kerning--;
 			jQuery(activeEl).css('margin-left', kerning);
 			adjustments[jQuery(activeEl).attr("id")] = kerning; // add/modify the current letter's kerning information to the "adjustments" object.
-//			jQuery("#kernjs_panel textarea").val(generateCSS(adjustments, unit, increment));
+			generateCSS(adjustments, unit, increment);
 		}
 		if(event.which === 39) { // If right arrow key
 			kerning++;
 			jQuery(activeEl).css('margin-left', kerning);
 			adjustments[jQuery(activeEl).attr("id")] = kerning; // add/modify the current letter's kerning information to the "adjustments" object.
 			generateCSS(adjustments, unit, increment);
-//			jQuery("#kernjs_panel textarea").val(generateCSS(adjustments, unit, increment));
 		}
 	});
 }
@@ -76,7 +78,6 @@ function generateCSS(adjustments, unit, increment) {
 				'}'
 				].join('\n');
 				theCSS = theCSS + '\n' + concatCSS;
-//				console.log(theCSS);
 		}
 	}
 	return theCSS;
