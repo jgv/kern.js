9ee20cda804df12d202a9f5f325f2bc6c6b40cd6
Brendan Stromberger <brendanstromberger@gmail.com>
2011-03-06 17:38:37 -0800

    Preparing for 0.0.1 release

--- kern.js
+++ kern.js
@@ -1,3 +1,14 @@
+/*
+* Kern.JS 0.1
+
+* Copyright 2011, Brendan Stromberger, www.brendanstromberger.com
+* Released under the WTFPL license 
+* http://sam.zoy.org/wtfpl/
+
+* Thanks to the Lettering.JS team for their amazing plugin and making the web a better place.
+* Date: Monday, March 7 2011
+*/
+
 if (typeof jQuery === 'undefined') {
 	var includejquery = document.createElement("script");
 	includejquery.src = "http://ajax.googleapis.com/ajax/libs/jquery/1.5/jquery.min.js";
@@ -16,7 +27,7 @@ function kern() {
 	thePanel =
 		['<style>',
 			'.kernjs_panel * { outline: none }',
-			'.kernjs_panel { position: relative; z-index: 1000000000; text-align: center; height: 40px; width: 100%; margin: 0 auto; background: -moz-linear-gradient(top, #45484d 0%, #000000 100%); background: -webkit-gradient(linear, left top, left bottom, color-stop(0%,#45484d), color-stop(100%,#000000));}',
+			'.kernjs_panel { position: absolute; top: 0; z-index: 1000000000; text-align: center; height: 40px; width: 100%; margin: 0 auto; background: -moz-linear-gradient(top, #45484d 0%, #000000 100%); background: -webkit-gradient(linear, left top, left bottom, color-stop(0%,#45484d), color-stop(100%,#000000)); border-bottom: 1px solid #333;}',
 			'.kernjs_panel .kernjs_button { padding-top: 20px; }',
 			'.kernjs_button .btn { display: inline-block; -webkit-border-radius: 8px; -moz-border-radius: 8px; border-radius: 8px; -webkit-box-shadow: 0 8px 0 #abad4f, 0 15px 20px rgba(0,0,0,.2); -moz-box-shadow: 0 8px 0 #abad4f, 0 15px 20px rgba(0,0,0,.2); box-shadow: 0 8px 0 #abad4f, 0 15px 20px rgba(0,0,0,.); -webkit-transition: -webkit-box-shadow .1s ease-in-out; -moz-transition: -moz-box-shadow .1s ease-in-out; -o-transition: -o-box-shadow .1s ease-in-out; transition: box-shadow .1s ease-in-out; }',
 			'.kernjs_button .btn span { display: inline-block; padding: 9px 20px; text-shadow: 0 -1px 1px rgba(255,255,255,.8); background: #e5e696; background: -moz-linear-gradient(top, #e5e696 0%, #d1d360 100%); background: -webkit-gradient(linear, left top, left bottom, color-stop(0%,#e5e696), color-stop(100%,#d1d360)); -webkit-border-radius: 8px; -moz-border-radius: 8px; border-radius: 8px; -webkit-box-shadow: inset 0 -1px 1px rgba(255,255,255,.15); -moz-box-shadow: inset 0 -1px 1px rgba(255,255,255,.15); box-shadow: inset 0 -1px 1px rgba(255,255,255,.15); -webkit-transition: -webkit-transform .2s ease-in-out; -moz-transition: -moz-transform .2s ease-in-out; -o-transition: -o-transform .2s ease-in-out; transition: transform .2s ease-in-out; }',
@@ -50,10 +61,10 @@ function kern() {
 			jQuery(this).attr('unselectable', 'on').css('-moz-user-select', 'none').each(function() { this.onselectstart = function() { return false; }; } );
 			jQuery(el).children().css('opacity', '.5');
 			jQuery(this).mousedown(function(event) { // Listens for clicks on the newly created span objects.
-				if(previousColor!==0) { jQuery(activeEl).css('color', previousColor); }
+				if(previousColor!==0) { jQuery(activeEl).css('color', previousColor).css('opacity', .5); }
 				activeEl = event.target; // Set activeEl to represent the clicked letter.
 				previousColor = jQuery(activeEl).css('color');
-				jQuery(activeEl).css('color', 'red');
+				jQuery(activeEl).css('color', '#00baff').css('opacity', 1);
 				lastX = event.pageX;
 				if(typeof(adjustments[jQuery(activeEl).attr("class")]) === 'undefined')
 				{
@@ -102,8 +113,8 @@ function kern() {
 	var outputPanel = jQuery(".kernjs_panel a").mouseup(function() {
 		var outputPanel = [
 			'<style>',
-				'.kernjs_overlay { position: absolute; height: 100%; width: 100%; z-index: 1000000001; overflow: none; background: rgba(0,0,0,.8); opacity: 0; font-family: Georgia; color: #222; }',
-				'.kernjs_container { background: #EEE; margin: 0 auto; width: 570px; margin-top: 40px; -webkit-border-radius: 0 0 5px 5px; border-radius: 0 0 5px 5px; }',
+				'.kernjs_overlay { position: absolute; top: 0; height: 100%; width: 100%; z-index: 100000000000000; overflow: none; background: rgba(0,0,0,.8); opacity: 0; font-family: Georgia; color: #222; }',
+				'.kernjs_container { background: #EEE; margin: 0 auto; width: 570px; -webkit-border-radius: 0 0 5px 5px; border-radius: 0 0 5px 5px; }',
 				'.kernjs_instructions { margin: 0 auto; text-align: center; padding: 40px; }',
 				'.kernjs_p { font-size: 18px; line-height: 24px; color: #555; text-align: left; text-shadow: 0 -1px 1px rgba(255,255,255,.8) }',
 				'.kernjs_instructions a { color: #009bd5 !important; text-align: center !important }',
@@ -119,7 +130,7 @@ function kern() {
 			'<div class="kernjs_overlay">',
 				'<div class="kernjs_container">',
 					'<div class="kernjs_instructions">',
-						'<div class="kernjs_p">Looks awesome. Here\'s the CSS for your lovely letters. Paste the following CSS into a stylesheet and include it in your page, then use the wonderfully easy-to-use <a class="kernjs_style" href="http://www.letteringjs.com\">Lettering.JS</a> to create the necessary style hooks.</p>',
+						'<div class="kernjs_p">Looks awesome. Here\'s the CSS for your lovely letters. Paste the following CSS into a stylesheet and include it in your page, then use the wonderfully easy-to-use <a class="kernjs_style" href="http://www.letteringjs.com\">Lettering.JS</a> to create the necessary style hooks.</p><br/>',
 						'<textarea>',
 							generateCSS(adjustments),
 						'</textarea>',
