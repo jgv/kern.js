2bfdee4c19c0a6db9723355d09975a5ef2279ae5
jgv <jvingiano@gmail.com>
2011-03-09 00:49:02 -0500

    appending the output panel to body

--- kern.js
+++ kern.js
@@ -211,7 +211,7 @@
 						outputHtml += "</div>";
 
             outputPanel.innerHTML = outputHtml;            
-            document.getElementById("kernjs").appendChild(outputHtml);
+            document.getElementById("kernjs").appendChild(document.body);
 
 		        jQuery(".kernjs_overlay").animate({ "opacity": 1 }, function() {
 			          // callback function here if we want to add any animations for the overlayed content later
